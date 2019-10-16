cd388971d3a4832f9cc2d2bf4486bd01ca3e7344
Josh Terrill <joshterrill.dev@gmail.com>
2019-10-16 00:33:59 -0700

    removed unnecessary handlebars include and used static html file instead

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,8 @@ use providers::{verify_green_life, verify_patient_id_center};
 use rocket::http::RawStr;
 use rocket::{get, routes};
 use rocket_contrib::json::{Json, JsonValue};
-use rocket_contrib::templates::Template;
-use std::collections::HashMap;
 use utilities::ResultToJson;
-
-#[get("/")]
-fn index() -> Template {
-    let version = env!("CARGO_PKG_VERSION");
-    let mut context = HashMap::new();
-    context.insert("version", version);
-    Template::render("index", &context)
-}
+use rocket_contrib::serve::StaticFiles;
 
 #[get("/<rec_id>")]
 fn green_life(rec_id: &RawStr) -> Json<JsonValue> {
@@ -33,7 +24,6 @@ fn main() {
     rocket::ignite()
         .mount("/greenlife", routes![green_life])
         .mount("/patientidcenter", routes![patient_id_center])
-        .mount("/", routes![index])
-        .attach(Template::fairing())
+        .mount("/", StaticFiles::from("static"))
         .launch();
 }
